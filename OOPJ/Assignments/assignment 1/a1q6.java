6c679eadefe41863117a9c87020ec48aa0c45dd3
ar.exe <78655673+ananthan13579@users.noreply.github.com>
2024-09-03 17:22:00 +0530

    Add files via upload

---
+++ OOPJ/Assignments/assignment 1/a1q6.java
@@ -0,0 +1,122 @@
+class hello{
+	public static void main(String[] args){
+		System.out.println("Hello, World!");
+	}
+}
+/*
+following generated in terminal by using -verbose command
+[parsing started SimpleFileObject[path]]
+[parsing completed 14ms]
+[loading /modules/java.base/module-info.class]
+[loading /modules/jdk.charsets/module-info.class]
+[loading /modules/jdk.sctp/module-info.class]
+[loading /modules/jdk.management/module-info.class]
+[loading /modules/jdk.editpad/module-info.class]
+[loading /modules/jdk.net/module-info.class]
+[loading /modules/java.datatransfer/module-info.class]
+[loading /modules/jdk.jconsole/module-info.class]
+[loading /modules/jdk.localedata/module-info.class]
+[loading /modules/jdk.zipfs/module-info.class]
+[loading /modules/java.desktop/module-info.class]
+[loading /modules/jdk.crypto.mscapi/module-info.class]
+[loading /modules/jdk.naming.dns/module-info.class]
+[loading /modules/java.se/module-info.class]
+[loading /modules/java.security.sasl/module-info.class]
+[loading /modules/jdk.xml.dom/module-info.class]
+[loading /modules/java.instrument/module-info.class]
+[loading /modules/jdk.jdi/module-info.class]
+[loading /modules/jdk.jcmd/module-info.class]
+[loading /modules/jdk.security.jgss/module-info.class]
+[loading /modules/jdk.jstatd/module-info.class]
+[loading /modules/java.scripting/module-info.class]
+[loading /modules/java.net.http/module-info.class]
+[loading /modules/jdk.internal.vm.compiler/module-info.class]
+[loading /modules/jdk.crypto.cryptoki/module-info.class]
+[loading /modules/jdk.management.agent/module-info.class]
+[loading /modules/jdk.internal.jvmstat/module-info.class]
+[loading /modules/jdk.jsobject/module-info.class]
+[loading /modules/java.xml/module-info.class]
+[loading /modules/jdk.incubator.vector/module-info.class]
+[loading /modules/jdk.attach/module-info.class]
+[loading /modules/java.sql/module-info.class]
+[loading /modules/jdk.internal.le/module-info.class]
+[loading /modules/java.management/module-info.class]
+[loading /modules/java.smartcardio/module-info.class]
+[loading /modules/jdk.dynalink/module-info.class]
+[loading /modules/jdk.random/module-info.class]
+[loading /modules/jdk.jdwp.agent/module-info.class]
+[loading /modules/jdk.jdeps/module-info.class]
+[loading /modules/jdk.unsupported.desktop/module-info.class]
+[loading /modules/jdk.security.auth/module-info.class]
+[loading /modules/jdk.hotspot.agent/module-info.class]
+[loading /modules/java.rmi/module-info.class]
+[loading /modules/jdk.jpackage/module-info.class]
+[loading /modules/jdk.internal.ed/module-info.class]
+[loading /modules/jdk.compiler/module-info.class]
+[loading /modules/jdk.javadoc/module-info.class]
+[loading /modules/java.management.rmi/module-info.class]
+[loading /modules/jdk.jlink/module-info.class]
+[loading /modules/jdk.jartool/module-info.class]
+[loading /modules/java.transaction.xa/module-info.class]
+[loading /modules/java.security.jgss/module-info.class]
+[loading /modules/jdk.unsupported/module-info.class]
+[loading /modules/java.compiler/module-info.class]
+[loading /modules/jdk.crypto.ec/module-info.class]
+[loading /modules/jdk.jshell/module-info.class]
+[loading /modules/java.naming/module-info.class]
+[loading /modules/jdk.internal.opt/module-info.class]
+[loading /modules/java.sql.rowset/module-info.class]
+[loading /modules/jdk.management.jfr/module-info.class]
+[loading /modules/java.logging/module-info.class]
+[loading /modules/jdk.internal.vm.ci/module-info.class]
+[loading /modules/jdk.naming.rmi/module-info.class]
+[loading /modules/jdk.internal.vm.compiler.management/module-info.class]
+[loading /modules/jdk.nio.mapmode/module-info.class]
+[loading /modules/java.xml.crypto/module-info.class]
+[loading /modules/jdk.httpserver/module-info.class]
+[loading /modules/jdk.accessibility/module-info.class]
+[loading /modules/java.prefs/module-info.class]
+[loading /modules/jdk.jfr/module-info.class]
+[search path for source files: .]
+[search path for class files: C:\Program Files\Java\jdk-21\lib\modules,.]
+[loading /modules/java.base/java/lang/Object.class]
+[loading /modules/java.base/java/lang/String.class]
+[loading /modules/java.base/java/lang/Deprecated.class]
+[loading /modules/java.base/java/lang/annotation/Retention.class]
+[loading /modules/java.base/java/lang/annotation/RetentionPolicy.class]
+[loading /modules/java.base/java/lang/annotation/Target.class]
+[loading /modules/java.base/java/lang/annotation/ElementType.class]
+[checking hello]
+[loading /modules/java.base/java/io/Serializable.class]
+[loading /modules/java.base/java/lang/AutoCloseable.class]
+[loading /modules/java.base/java/lang/System.class]
+[loading /modules/java.base/java/io/PrintStream.class]
+[loading /modules/java.base/java/lang/Appendable.class]
+[loading /modules/java.base/java/io/Closeable.class]
+[loading /modules/java.base/java/io/FilterOutputStream.class]
+[loading /modules/java.base/java/io/OutputStream.class]
+[loading /modules/java.base/java/io/Flushable.class]
+[loading /modules/java.base/java/lang/Comparable.class]
+[loading /modules/java.base/java/lang/CharSequence.class]
+[loading /modules/java.base/java/lang/constant/Constable.class]
+[loading /modules/java.base/java/lang/constant/ConstantDesc.class]
+[wrote hello.class]
+[total 177ms]
+*/
+/*
+Compiled from "q6.java"
+class hello {
+  hello();
+    Code:
+       0: aload_0
+       1: invokespecial #1                  // Method java/lang/Object."<init>":()V
+       4: return
+
+  public static void main(java.lang.String[]);
+    Code:
+       0: getstatic     #7                  // Field java/lang/System.out:Ljava/io/PrintStream;
+       3: ldc           #13                 // String Hello, World!
+       5: invokevirtual #15                 // Method java/io/PrintStream.println:(Ljava/lang/String;)V
+       8: return
+}
+*/
