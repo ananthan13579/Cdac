860726fa7209874a3b07e1f9679b31dc036eb31a
ar.exe <78655673+ananthan13579@users.noreply.github.com>
2024-09-08 22:03:25 +0530

    Update Program2.java

--- OOPJ/Assignments/Assignment 3/methodProblems/Program2.java
+++ OOPJ/Assignments/Assignment 3/methodProblems/Program2.java
@@ -5,7 +5,7 @@ class CompoundInterestCalculator{
 Currency c=Currency.getInstance("INR");
 Scanner sc=new Scanner(System.in);
 private int initialInv;
-private int annualInterestRate;
+private double annualInterestRate;
 private int numOfCompounds;
 private int duration;
 private double futureValue;
@@ -13,14 +13,14 @@ public void acceptRecord() {
 System.out.print("Enter initial investment amount: ");
 initialInv=sc.nextInt();
 System.out.print("Enter annual interest rate: ");
-annualInterestRate=sc.nextInt();
+annualInterestRate=sc.nextDouble();
 System.out.print("Enter how many times the interest is compounded: ");
 numOfCompounds=sc.nextInt();
 System.out.print("Enter the duration in years: ");
 duration=sc.nextInt();
 }
 public void calculateFutureValue() {
-futureValue=initialInv*Math.pow((1+annualInterestRate)/numOfCompounds,numOfCompounds*duration);
+futureValue=initialInv*Math.pow(1+(annualInterestRate/(100*numOfCompounds)),numOfCompounds*duration);
 }
 public void printRecord() {
 System.out.println("Future value earned is= "+c.getSymbol()+String.format("%.3f",futureValue));
